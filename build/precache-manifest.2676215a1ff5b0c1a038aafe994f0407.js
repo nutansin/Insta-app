8036062b8315e4806bac53d4f2ed7f9f762d8893
nutan singh <nutansingh611@gmail.com>
2020-05-24 21:15:55 +0530

    issue fixed

---
+++ build/precache-manifest.2676215a1ff5b0c1a038aafe994f0407.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b32903404e4d6bbde505",
+    "url": "/static/css/main.4160dcab.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "984e8be6528ef7edee3c",
+    "url": "/static/js/2.984e8be6.chunk.js"
+  },
+  {
+    "revision": "da1a1de0bc1bf700ebcd24cb1557293a",
+    "url": "/static/media/like_icon.da1a1de0.png"
+  },
+  {
+    "revision": "b32903404e4d6bbde505",
+    "url": "/static/js/main.b3290340.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "984e8be6528ef7edee3c",
+    "url": "/static/css/2.fa73624d.chunk.css"
+  },
+  {
+    "revision": "3c1a9adcf3f5b0123733d3af59e44e7c",
+    "url": "/index.html"
+  }
+];
--- src/store.js
+++ src/store.js
@@ -1,16 +1,15 @@
 import { applyMiddleware, createStore, compose } from 'redux'
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import thunkMiddleware from 'redux-thunk';
+import combineReducer from './reducers';
 
 const initialState = {};
-const middleware = [thunk];
+
 const store = createStore(
-    rootReducer, 
-        initialState, 
-        compose(
-            applyMiddleware(...middleware),
-            window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-        )
-    );
+    combineReducer,
+    initialState,
+    compose(
+        applyMiddleware(thunkMiddleware)
+    )
+);
 
 export default store;
